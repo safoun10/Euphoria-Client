a9e65c2ac16338de5d4e5b6043c42621e64882cb
Safoun <safoun.official@gmail.com>
2023-06-06 23:22:52 +0600

    initial commit of euphoria client

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div className="fs-4">Hello world!</div>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
