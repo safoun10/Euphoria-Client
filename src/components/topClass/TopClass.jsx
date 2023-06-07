c1acc902c44ae542e08817a558c9898c6f53e8a6
Safoun <safoun.official@gmail.com>
2023-06-07 09:24:02 +0600

    adding new section

--- src/components/topClass/TopClass.jsx
+++ src/components/topClass/TopClass.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./TopClass.css"
 
 const TopClass = () => {
     return (
 		<div>
 			<div className="text-center display-1 mt-5 pb-1 text-cursive">
-				Visit our top Photography classes
+				Visit our top <span className='text-brown'>Photography</span> classes
 			</div>
-			<div className='text-center w-75 mx-auto'>
+			<div className="text-center w-75 mx-auto">
 				Unlock your creative potential and capture stunning images with
 				our premier photography classes. From portrait to landscape,
 				wildlife to automobile, explore the art of photography with our
