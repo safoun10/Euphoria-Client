60b5bd0314aa30e8c22772b8e95303486652abe1
Safoun <safoun.official@gmail.com>
2023-06-07 06:34:24 +0600

    added basic requirements

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div className="fs-4">Hello world!</div>,
+		element: (
+			<div className="fs-4">
+				No one shall be subjected to arbitrary arrest, detention or
+				exile. Everyone is entitled in full equality to a fair and
+				public hearing by an independent and impartial tribunal, in the
+				determination of his rights and obligations and of any criminal
+				charge against him. No one shall be subjected to arbitrary
+				interference with his privacy, family, home or correspondence,
+				nor to attacks upon his honour and reputation. Everyone has the
+				right to the protection of the law against such interference or
+				attacks.
+			</div>
+		),
 	},
 ]);
 
