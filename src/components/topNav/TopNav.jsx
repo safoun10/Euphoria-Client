6d941ef048aa169277b1783af87c1b22e17f92b0
Safoun <safoun.official@gmail.com>
2023-06-07 15:03:14 +0600

    topnav updated

--- src/components/topNav/TopNav.jsx
+++ src/components/topNav/TopNav.jsx
@@ -7,22 +7,25 @@ const TopNav = () => {
 		<div className="mx-auto py-2 text-white" style={{ maxWidth: "1400px" }}>
 			<div className="d-flex justify-content-between align-items-center">
 				<div className="display-6">EUPHORIA</div>
-				<div className="d-flex gap-3">
+				<div className="d-flex gap-4">
 					<div>
-						<Link className="text-decoration-none text-white">link</Link>
+						<Link className="text-decoration-none text-white fs-5 hover-link">Home</Link>
 					</div>
 					<div>
-						<Link className="text-decoration-none text-white">link</Link>
+						<Link className="text-decoration-none text-white fs-5 hover-link">Instructors</Link>
 					</div>
 					<div>
-						<Link className="text-decoration-none text-white">link</Link>
+						<Link className="text-decoration-none text-white fs-5 hover-link">Classes</Link>
+					</div>
+					<div>
+						<Link className="text-decoration-none text-white fs-5 hover-link">Dashboard</Link>
 					</div>
 				</div>
 				<div>
 					<Link
-						className="text-decoration-none btn text-white"
+						className="text-decoration-none btn text-white btn btn-outline-secondary rounded-0 px-5 border-3"
 					>
-						login
+						Login
 					</Link>
 				</div>
 			</div>
