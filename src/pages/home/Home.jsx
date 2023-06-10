353b47599e03577d81d57da573f177824f147168
Safoun <safoun.official@gmail.com>
2023-06-10 07:44:55 +0600

    added theme in features section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SwiperBanner from '../../components/swiperBanner/SwiperBanner';
 import TopNav from '../../components/topNav/TopNav';
 import TopClass from '../../components/topClass/TopClass';
 import Features from '../../components/features/Features';
 import Footer from '../../components/footer/Footer';
+import { ThemeContext } from '../../providers/ThemeProvider';
 
 const Home = () => {
+	const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
     return (
 		<div>
 			<div className="bg-char">
 				<TopNav></TopNav>
 			</div>
 			<SwiperBanner></SwiperBanner>
-			<div className='bg-brown'>
+			<div className={`${isDarkTheme ? "bg-char" : "bg-brown"}`}>
 				<Features></Features>
 			</div>
 			<TopClass></TopClass>
